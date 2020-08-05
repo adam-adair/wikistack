e8c1ea424195a26a8ed107e53f7f8da350deabc3
djgallag <djgallag@umich.edu>
2020-08-04 20:41:40 -0400

    main.js is running

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const override = require('method-override');
+const views = require('./views');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(override('X-HTTP-Method-Override'))
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send(views.main(''));
 })
 
 app.listen(3000, () => {
